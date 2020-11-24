8f8555742bcaed6015fea399f08dd1a0595b6c87
Thiago <thiagocostas2017@gmail.com>
2020-11-24 17:18:06 -0300

    completo

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace exc4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+            double[] numeros = new double [10];
+
+            double min = 10000;
+            double max = 0;
+
+            for (int i = 0; i < 10; i++)
+            {   Console.WriteLine();
+                Console.WriteLine($"Digite o {i+1}° número: ");
+                Console.WriteLine();
+                numeros[i] = double.Parse(Console.ReadLine());
+                if (numeros[i] > max) {      
+                    max = numeros[i]; }            
+                if (numeros[i] < min) {      
+                    min = numeros[i]; }
+            }
+                   
+                Console.WriteLine(" Entre os 10 valores digitados o menor é: ");
+                Console.Write(min);
+                Console.WriteLine();
+                Console.WriteLine(" Entre os 10 valores digitados o maior é: ");
+                Console.Write(max);
+                Console.WriteLine();
+            }
+        }
+    }
+}
